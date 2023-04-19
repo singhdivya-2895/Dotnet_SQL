74f56e17c44da1cf54f7cf93a88a089e65ba20d9
Divya Singh <myadmin@divya-vs-2022>
2023-04-19 10:35:03 +0000

    More changes for Get, Post Route

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Mapper;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();
+            var regionsDto = MappingHelper.mapRegionDomainListintoRegionDtoList(regions);
+            return Ok(regionsDto);
+        }
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById(Guid id) 
+        {
+            var region = dbContext.Regions.Find(id);
+            {
+                if (region == null)
+                {
+                  return NotFound();
+                }
+                var regionsDto = MappingHelper.mapRegionDomaintoRegionDto(region);
+                return Ok(region);
+            }
+        }
+        [HttpPost]
+        public IActionResult Create(AddRegionRequestDto addRegionRequestDto) 
+        {
+            //map Dto to Domainmodel
+            var regionDomainModel = MappingHelper.mapAddRegionRequestDtointoRegionModel(addRegionRequestDto);
+           
+            dbContext.Regions.Add(regionDomainModel);
+            dbContext.SaveChanges();
+           
+            //map domain model to Dto
+            var regionDto = MappingHelper.mapRegionDomaintoRegionDto(regionDomainModel);  
+            return CreatedAtAction(nameof(GetById),new { id = regionDto.Id}, regionDto);
+        }
+    }
+}
---
+++ NZWalks.API/Mapper/Mapper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Identity.Client;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Mapper
+{
+    public static class MappingHelper
+    {
+       
+        public static RegionDto mapRegionDomaintoRegionDto(Region regionDomainModal)
+        {
+            return new RegionDto
+            {
+                Id = regionDomainModal.Id,
+                Code = regionDomainModal.Code,
+                Name = regionDomainModal.Name,
+                RegionImageUrl = regionDomainModal.RegionImageUrl,
+            };
+        }
+
+        public static List<RegionDto> mapRegionDomainListintoRegionDtoList(List<Region> regionDomainModalList)
+        {
+            return regionDomainModalList
+                .Select(x => mapRegionDomaintoRegionDto(x))
+                .ToList();
+        }
+        public static Region mapAddRegionRequestDtointoRegionModel(AddRegionRequestDto regionRequestDto) 
+        {
+            //map Dto to DomainModel
+            return new Region
+            {
+              Code = regionRequestDto.Code,
+              Name = regionRequestDto.Name,
+              RegionImageUrl = regionRequestDto.RegionImageUrl,
+            };
+            
+        }
+    } 
+}
+    
