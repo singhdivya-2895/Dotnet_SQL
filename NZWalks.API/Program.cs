554ef515b0138e282a377bc82c5bcfb558668091
Divya Singh <myadmin@divya-vs-2022>
2023-05-08 07:24:13 +0000

    Role based Authorization, Add Authorization to Swagger

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,8 +1,10 @@
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using NZWalks.API.Data;
+using Microsoft.OpenApi.Models;
 using NZWalks.API.Repositories;
 using System.Text;
 
@@ -13,7 +15,35 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "NZ Walks Api - Pankaj Loves Divya", Version = "v1" });
+    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = JwtBearerDefaults.AuthenticationScheme
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+           new OpenApiSecurityScheme
+           {
+              Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = JwtBearerDefaults.AuthenticationScheme
+              },
+             Scheme = "Oauth2",
+             Name = JwtBearerDefaults.AuthenticationScheme,
+             In = ParameterLocation.Header
+           },
+            new List<string>()
+        }
+    });
+});
+
 builder.Services.AddFluentValidation(options => options.RegisterValidatorsFromAssemblyContaining<Program>());
 
 builder.Services.AddDbContext<NZWalksDbContext>(
--- NZWalks.Identity/Program.cs
+++ NZWalks.Identity/Program.cs
@@ -12,11 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options => 
-{    
- options.SwaggerDoc("v1", new OpenApiInfo(){Title = "NZ Walks Identity", Version = "v!"});
-});
-
+builder.Services.AddSwaggerGen();
 builder.Services.AddAuthentication();
 builder.Services.AddScoped<ITokenRepository,TokenRepository>();
 builder.Services.AddDbContext<NZWalksAuthDbContext>(
